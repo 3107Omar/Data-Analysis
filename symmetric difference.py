473794e5ffb3955add9882439c3036ea61f2b511
3107Omar <51252170+3107Omar@users.noreply.github.com>
2020-07-20 12:01:20 +0200

    Update symmetric difference.py

--- symmetric difference.py
+++ symmetric difference.py
@@ -8,35 +8,11 @@ Created on Mon Apr 27 14:42:16 2020
 
 import pandas as pd
 
-df=pd.read_csv("/home/ahmedomar/Desktop/Kibana.csv")
-df
-df1=pd.read_csv("/home/ahmedomar/Desktop/EDR28.csv")
-df1
-del df
-del df1
+df=pd.read_csv("/path/to/1.csv")
+df1=pd.read_csv("/path/to/2.csv")
 
+df2=pd.merge(df, df1, on=["column1", "column2"], how='outer', indicator=True) ## compare between 2 different csv files
+## on column1 and coulmn2 and print the symmetric difference between them or print values which are not intersected then indicate which of the values found in 
+## coulmn1 or column2
 
-df3=pd.merge(df, df1, on=["msisdn", "darwinLoanId"])
-df3
-df.count()
-df1.count()
-df3.count()
-
-df3=df3.astype(str)
-df3
-df4=df3.to_csv("/home/ahmedomar/Desktop/test.csv")
-
-df5=pd.merge(df, df1, on=["msisdn", "darwinLoanId"], how='outer', indicator=True)
-df5
-df5=df5.astype(str)
-df5
-df5.count()
-
-df6=df5.to_csv("/home/ahmedomar/Desktop/test2.csv")
-
-df7=df5[~df5._merge.str.contains("both")]
-df7
-df8=df7.to_csv("/home/ahmedomar/Desktop/test4.csv")
-
-
-clear
+df3=df2.to_csv("/path/to/output location.csv")
